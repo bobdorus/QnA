7fddfd132914ce56025364c1ca6524fbf50342a4
kaigitwork <natevictorlee336@outlook.com>
2024-05-14 15:09:22 +0800

    modularize question display

--- qna_app.py
+++ qna_app.py
@@ -3,42 +3,44 @@ from snowflake.snowpark.functions import col
 import pandas as pd
 import random
 
-"""
-Question:
-    # Q_NUM
-    # Q_TEXT
-    # TOPIC
-    # CORRECT_ANSWER
-    # COMMENT
-"""
+# """
+# Question:
+#     # Q_NUM
+#     # Q_TEXT
+#     # TOPIC
+#     # CORRECT_ANSWER
+#     # COMMENT
+# """
 
+def question_display(q_num, session): 
+    st.subheader("Question Details:")
+    selected_num = int(q_num) if q_num and 0 < int(q_num) < 1100 else 1
+
+    my_dataframe = session.table("qna.pro.question").filter(col("Q_NUM") == selected_num)
+
+    # Handling missing question
+    if my_dataframe.count() == 0:
+        st.warning("Question not found.")
+    return  # Exit the function to prevent errors
+
+    pd_df = my_dataframe.toPandas()
+    st.write(selected_num)
+    st.write(pd_df['Q_TEXT'][0])
+
+    options_df = session.table("qna.pro.options").filter(col("Q_NUM") == selected_num).toPandas()
+    st.dataframe(options_df)
 
 def review_mode(q_num, session):
     with st.container():  # Creates a container for Review mode
-        st.subheader("Question Details:")
-        selected_num = int(q_num) if q_num and 0 < int(q_num) < 1100 else 1
-
-        my_dataframe = session.table("qna.pro.question").filter(col("Q_NUM") == selected_num)
-
-        # Handling missing question
-        if my_dataframe.count() == 0:
-            st.warning("Question not found.")
-            return  # Exit the function to prevent errors
-
-        pd_df = my_dataframe.toPandas()
-        st.write(selected_num)
-        st.write(pd_df['Q_TEXT'][0])
-
-        options_df = session.table("qna.pro.options").filter(col("Q_NUM") == selected_num).toPandas()
-        st.dataframe(options_df)
+        question_display(q_num, session)
 
 def seq_mode(q_num, session):
     with st.container():  # Creates a container for Sequence mode
-        st.write("implement later")
+        question_display(q_num, session)
 
 def test_mode(q_num, session):
     with st.container():  # Creates a container for Test mode
-        st.write("implement later")
+        question_display(q_num, session)
 
 # Main App
 st.title(":snowflake: Question & Answer App :snowflake:")
