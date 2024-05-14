ca22623ff6f87cdaa433037510d6553113734ad4
kaigitwork <natevictorlee336@outlook.com>
2024-05-15 00:06:41 +0800

    fix error

--- qna_app.py
+++ qna_app.py
@@ -7,9 +7,9 @@ MAX = 1099
 
 def get_option_selector(session, q_num):
     options_df = session.table("qna.pro.options").filter(col("Q_NUM") == q_num).toPandas()
-    correct_answer_len = len(session.table("qna.pro.question").filter(col("Q_NUM") == q_num).select("CORRECT_ANSWER")).collect()[0][0]
+    correct_answer = session.table("qna.pro.question").filter(col("Q_NUM") == q_num).select("CORRECT_ANSWER").collect()[0][0]
 
-    if correct_answer_len == 1:
+    if len(correct_answer) == 1:
         selected_option = st.radio("Select an option", [f"{option}: {text}" for option, text in zip(options_df["OPTION"], options_df["TEXT"])])
         selected_options = [selected_option]
     else:
