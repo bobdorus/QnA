60d7d8d6af9e9cfaaa11d309e2bd6bcd191abdd9
kaigitwork <natevictorlee336@outlook.com>
2024-07-01 23:02:01 +0800

    change test app table's name

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import col
 import pandas as pd
 import datetime
-change_log_df = session.table("QNA.pro.UPDATE_LOG_TBL").filter(col("Q_NUM") == selected_num).toPandas()
+change_log_df = session.table("QNA_DB.pro.Question").filter(col("Q_NUM") == selected_num).toPandas()
 st.dataframe(change_log_df)
